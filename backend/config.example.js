bc6ece2e7d39da5c97964ce2e9cfbc730e834c76
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-04-14 09:50:00 +0530

    Backend Initial commit

---
+++ backend/config.example.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = "";
---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(200).send("Welcome To MERN Stack Tutorial");
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
